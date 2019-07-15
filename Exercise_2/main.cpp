3f12bd1fe7c0cafb9ed4d8c6454b88b1efa77a28
sen9a <sen9a1990@gmail.com>
2019-07-15 12:55:07 +0300

    Fix task 2 according comments

--- Exercise_2/main.cpp
+++ Exercise_2/main.cpp
@@ -5,12 +5,13 @@
 #include <algorithm>
 #include <vector>
 #include <limits>
+#include <unordered_set>
 #include <climits>
 #include <list>
+#include <bits/unordered_set.h>
 
-using namespace std;
 
-struct nodes {
+struct edge {
     int start_node;
     int end_node;
     int distance;
@@ -23,37 +24,34 @@ struct table {
 };
 
 
-vector<int> create_set_of_values_from_file(const vector<nodes> &all_data){
-    vector<int> set_of_values = {};
-    for(int i=0; i < all_data.size(); ++i){
-        if(find(set_of_values.begin(), set_of_values.end(), all_data[i].start_node) == set_of_values.end()){
-            set_of_values.push_back(all_data[i].start_node);
-        }
+std::unordered_set<int> gather_node_indices(const std::vector<edge> &all_data){
+    std::unordered_set<int> values;
+    for(auto i : all_data){
+        values.insert(i.start_node);
     }
-    return set_of_values;
+    return values;
 }
 
-vector<table> create_table_from_file(const vector<int> &set_of_values, int start_node){
-    vector<table> result_table = {};
+std::vector<table> create_table_from_file(const std::unordered_set<int> &set_of_values, int start_node){
+    std::vector<table> result_table = {};
     table row = {};
-    for (int i=0;i <set_of_values.size(); ++i) {
-        if (set_of_values[i] == start_node) {
-            row = {set_of_values[i], 0, INT_MAX};
+    for (auto i : set_of_values) {
+        if (i == start_node) {
+            row = {i, 0, std::numeric_limits<int>::max()};
         } else {
-            row = {set_of_values[i], INT_MAX, INT_MAX};
+            row = {i, std::numeric_limits<int>::max(), std::numeric_limits<int>::max()};
         }
         result_table.push_back(row);
     }
     return result_table;
 }
 
-vector<nodes> read_from_file(const string &file){
-    string text;
-    vector<nodes> graph = {};
-    nodes node = {};
-    ifstream open_file(file);
+std::vector<edge> read_from_file(const std::string &file){
+    std::vector<edge> graph = {};
+    edge node = {};
+    std::ifstream open_file(file);
     if (!open_file){
-        cout << "File do not exists"<< endl;
+        std::cout << "File do not exists"<< std::endl;
         return graph;
     }
     while(open_file>>node.start_node>>node.end_node>>node.distance){
@@ -62,38 +60,57 @@ vector<nodes> read_from_file(const string &file){
     return graph;
 }
 
-table find_min_vector(const vector<int> &list_of_visited_nodes, vector<table> &main_table){
+
+bool check_if_value_is_in_container(const std::vector<int> &m_vector, int value){
+    return find(m_vector.begin(), m_vector.end(), value) == m_vector.end();
+}
+
+
+bool min_value_condition(const std::vector<int> &list_of_visited_nodes,const table &row, int min_value){
+    return check_if_value_is_in_container(list_of_visited_nodes, row.vertex) && row.distance < min_value;
+}
+
+table find_min_vector(const std::vector<int> &list_of_visited_nodes,const std::vector<table> &main_table){
     table min_value_node = {};
-    int min_value = INT_MAX;
-    for(int i = 0;i< main_table.size(); ++i){
-        if (find(list_of_visited_nodes.begin(), list_of_visited_nodes.end(), main_table[i].vertex) ==
-        list_of_visited_nodes.end() && main_table[i].distance < min_value){
-            min_value = main_table[i].distance;
-            min_value_node = main_table[i];
+    int min_value = std::numeric_limits<int>::max();
+    for(auto i:main_table){
+        if (min_value_condition(list_of_visited_nodes, i, min_value)){
+            min_value = i.distance;
+            min_value_node = i;
         }
     }
     return min_value_node;
 }
 
+bool check_if_make_changes_to_main_column(const edge &graph_data,
+                                          const table &main_table_row,
+                                          const table &current_node,
+                                          const std::vector<int> &visited){
+    return current_node.vertex == graph_data.start_node
+           && check_if_value_is_in_container(visited, graph_data.end_node)
+           && graph_data.end_node == main_table_row.vertex
+           && current_node.distance + graph_data.distance < main_table_row.distance;
+}
+
 void set_values_to_table(table current_node,
-                         const vector<nodes> &graph,
-                         vector<table> &main_table,
-                         vector<int> visited){
-    for(int i = 0; i < graph.size(); ++i){
-        for(int j = 0; j < main_table.size(); ++j){
-            if(current_node.vertex == graph[i].start_node
-               && find(visited.begin(), visited.end(), graph[i].end_node) == visited.end()
-               && graph[i].end_node == main_table[j].vertex
-               && current_node.distance+graph[i].distance < main_table[j].distance){
-                main_table[j].distance = graph[i].distance + current_node.distance;
-                main_table[j].previos_vector = graph[i].start_node;
+                         const std::vector<edge> &graph,
+                         std::vector<table> &main_table,
+                         const std::vector<int> &visited){
+    for(auto &i : graph){
+        for(auto &j : main_table){
+            if(check_if_make_changes_to_main_column(i,j,current_node,visited)){
+                j.distance = i.distance + current_node.distance;
+                j.previos_vector = i.start_node;
             }
         }
     }
 }
 
-list<table> shortest_path(const table &first_element, vector<table> &result_table, int start_node, int end_node){
-    list<table> graph_path = {};
+std::list<table> shortest_path(const table &first_element,
+                               const std::vector<table> &result_table,
+                               int start_node,
+                               int end_node){
+    std::list<table> graph_path = {};
     int i = 0;
     while(end_node != start_node){
         if(result_table[i].vertex == end_node){
@@ -109,14 +126,14 @@ list<table> shortest_path(const table &first_element, vector<table> &result_tabl
 }
 
 
-table fill_up_result_table(const vector<int> &unvisited_vector,
-                           vector<table> &result_table,
-                           vector<nodes> &graph
+table fill_up_result_table(const std::unordered_set<int> &unvisited_vector,
+                           std::vector<table> &result_table,
+                           std::vector<edge> &graph
                            ){
-    vector<int> visited_vector = {};
+    std::vector<int> visited_vector = {};
     table current_vertex = {};
     table first_element = {};
-    int size = unvisited_vector.size();
+    std::size_t size = unvisited_vector.size();
     while (visited_vector.size() != size) {
         current_vertex = find_min_vector(visited_vector, result_table);
         if (visited_vector.empty()) {
@@ -128,26 +145,21 @@ table fill_up_result_table(const vector<int> &unvisited_vector,
     return first_element;
 }
 
-int check_input(vector<int> set_of_values, bool start) {
+int check_input(const std::unordered_set<int> &set_of_values, const std::string& prompt) {
     int node = 0;
-    string display;
-    if(start){
-        display = "Start node: ";
-    }else{
-        display = "End node: ";
-    }
-    while (cout << display){
-        if (cin>>node){
-            if(find(set_of_values.begin(), set_of_values.end(), node) != set_of_values.end()){
-                break;
+    while (std::cout << prompt){
+        if (std::cin>>node){
+            std::unordered_set<int>::const_iterator it = set_of_values.find(node);
+            if(it != set_of_values.end()){
+                return node;
             }else{
-                cout << node<< " there is no such node, please try again"<<endl;
+                std::cout << node<< " there is no such node, please try again"<<std::endl;
             }
         }else{
-            cout << "You have wrote bad input, please type number "<< endl;
+            std::cout << "You have wrote bad input, please type number "<< std::endl;
         }
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(),'\n');
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
     }
     return node;
 }
@@ -155,25 +167,25 @@ int check_input(vector<int> set_of_values, bool start) {
 
 int main(int args, char **argv){
     if(args==2){
-        vector<nodes> graph = read_from_file(argv[1]);
+        std::vector<edge> graph = read_from_file(argv[1]);
         if (graph.empty()){
             return EXIT_FAILURE;
         }
-        vector<int> set_of_values = create_set_of_values_from_file(graph);
-        int start_node = check_input(set_of_values, true);
-        int end_node = check_input(set_of_values, false);
+        std::unordered_set<int> set_of_values = gather_node_indices(graph);
+        int start_node = check_input(set_of_values, "Start node: ");
+        int end_node = check_input(set_of_values, "End Node: ");
         auto result_table = create_table_from_file(set_of_values, start_node);
         table first_element = fill_up_result_table(set_of_values, result_table, graph);
-        list<table> finish = shortest_path(first_element, result_table, start_node, end_node);
+        std::list<table> finish = shortest_path(first_element, result_table, start_node, end_node);
         for(auto node_number: finish){
             if(finish.back().vertex != node_number.vertex) {
-                cout << node_number.vertex << "-->";
+                std::cout << node_number.vertex << "-->";
             }else{
-                cout << node_number.vertex << " total distance is "<< node_number.distance << endl;
+                std::cout << node_number.vertex << " total distance is "<< node_number.distance << std::endl;
             }
         }
         return EXIT_SUCCESS;
     }
-    cout << "Please give path to the file as 1 parameter"<<endl;
+    std::cout << "Please give path to the file as 1 parameter"<<std::endl;
     return EXIT_FAILURE;
 }
